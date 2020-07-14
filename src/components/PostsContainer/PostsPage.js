c8370014e94f47b66ee3d4c7e4d8d179b5e1ed33
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-14 06:44:27 -0400

    Complete PostsPage.js

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,17 +1,18 @@
-//Complete the necessary code in this file
-// import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+import dummyData from "./../../dummy-data";
 
 const PostsPage = () => {
-  // set up state for your data
+  const [postList, setPostList] = useState(dummyData);
+
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {postList.map((post) => <Post post = {post} />)}
     </div>
   );
 };
 
 export default PostsPage;
+
+//  LocalWords:  dummyData
