694f9de27c3be4624ce8f60c1fdfedeb7f002b81
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-15 01:11:07 -0400

    Complete App.App()

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-/* 
-Start here and work your way down the nested components.
-Not all files in the project need code added.
-Look at each file to see what props need to be passed.
-*/
-
-// Import the state hook
-import React from "react";
-// Import the Posts (plural!) and SearchBar components, since they are used inside App component
-// Import the dummyData
+import React, {useState} from "react";
+import Posts from "./components/Posts/Posts";
+import SearchBar from "./components/SearchBar/SearchBar";
+import dummyData from "./dummy-data";
 import "./App.css";
 
 const App = () => {
-  // Create a state called 'posts' to hold the list of posts, initializing to dummyData.
+  const [posts, setPosts] = useState(dummyData);
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
   const likePost = postId => {
@@ -25,8 +19,8 @@ const App = () => {
 
   return (
     <div className="App">
-      {/* Add SearchBar and Posts here to render them */}
-      {/* Check the implementation of each component, to see what props they require, if any! */}
+      <SearchBar />
+      <Posts likePost = {likePost} posts = {posts}/>
     </div>
   );
 };
