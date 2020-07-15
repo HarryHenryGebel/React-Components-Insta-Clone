713325d29616d51f2ed5c70ed4e3dc466f7a86ac
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-15 02:39:17 -0400

    Eliminate warning for not having a key

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Post from "./Post";
 import "./Posts.css";
+import {requester} from 'easier-requests';
 
 const Posts = (props) => {
   const {likePost, posts} = props;
 
   return (
     <div className="posts-container-wrapper">
-      {posts.map((post) => <Post likePost = {likePost} post={post}/>)}
+      {posts.map((post) => <Post key = {requester.createUniqueID()}
+                                 likePost = {likePost}
+                                 post={post}/>)}
     </div>
   );
 };
