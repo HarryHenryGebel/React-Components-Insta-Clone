c703b3541deb5dee263f97bde5fea2fd9c6ecda3
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-15 01:50:33 -0400

    Complete Comments.js

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Comment from './Comment';
 import './Comments.css';
+import {requester} from "easier-requests";
 
 const Comments = props => {
-  // Make sure the parent of Comments is passing the right props!
   const { comments } = props;
 
   return (
     <div>
-      {/* map through the comments data array and render a Comment for every comment piece of data */}
+      {comments.map((comment) =>
+                    <Comment key={requester.createUniqueID()}
+                             comment={comment}/>)}
     </div>
   );
 };
